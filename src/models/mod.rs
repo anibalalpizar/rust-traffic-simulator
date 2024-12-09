eb36fdae50cc1e85e1c72e8c4bd04efbd4d2d243
anibalalpizar <anibal.alpizar14@gmail.com>
2024-12-08 21:15:08 -0600

    Add models module with Intersection, Road, TrafficLight, and Vehicle

---
+++ src/models/mod.rs
@@ -0,0 +1,12 @@
+mod intersection;
+mod road;
+mod traffic_light;
+mod vehicle;
+
+pub use intersection::Intersection;
+pub use road::Road;
+pub use traffic_light::TrafficLight;
+pub use vehicle::Vehicle;
+
+pub use road::TrafficSignal;
+pub use vehicle::VehicleType;
